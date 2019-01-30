5bf8228a70fab11e7c94d0df0ef88f614e12f4fb
Shardul Malik <shardulmalik@gmail.com>
2019-01-30 21:32:51 +0530

    added basic functionalities of dmas

--- dmas.js
+++ dmas.js
@@ -1,7 +1,13 @@
-const sum = require ("./sum");
+const sum      = require ("./sum");
+const multiply = require ("./multiply");
+const divide   = require ("./divide");
+const subtract = require ("./subtract");
 
 const dmas = {};
 
-dmas.sum = sum;
+dmas.sum      = sum;
+dmas.multiply = multiply;
+dmas.divide   = divide;
+dmas.subtract = subtract;
 
 module.exports = dmas;

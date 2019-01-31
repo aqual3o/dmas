3d912fc21b46cc1482f2da03f32e4e0ede7a84cf
Shardul Malik <shardulmalik@gmail.com>
2019-01-31 20:15:09 +0530

    added test file

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const dmas = require ("../dmas");
+
+console.log (`Summing      2 & 4,    Expected : 6, Result : ${dmas.sum      (2, 4)}`);
+console.log (`Multiplying  2 & 4,    Expected : 8, Result : ${dmas.multiply (2, 4)}`);
+console.log (`Dividing     4 by 2,   Expected : 2, Result : ${dmas.divide   (4, 2)}`);
+console.log (`Subtracting  2 from 4, Expected : 2, Result : ${dmas.subtract (4, 2)}`);
+
+console.log (`Summing array  [2, 4, 6], Expected : 12, Result : ${dmas.sumArray ([2, 4, 6])}`);
